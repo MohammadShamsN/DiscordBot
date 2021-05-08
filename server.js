024f8f650f7bc81378aa4cbbf5963788755c93b5
Glitch (glitch-hello-node) <none>
2021-05-08 17:24:56 +0000

    💎🌦 Checkpoint ./server.js:31490453/3698 ./.glitch-assets:31490453/386

--- server.js
+++ server.js
@@ -1,86 +1,49 @@
-const path = require("path");
+const Discord = require("discord.js");
+const fs = require("fs");
+require("dotenv").config();
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  logger: true
+const express = require('express');
+const app = express();
+
+const client = new Discord.Client();
+
+const config = {
+  token: process.env.TOKEN,
+  prefix: process.env.PREFIX,
+};
+client.config = config;
+client.queue = new Map();
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
 });
 
-// Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`[Command Manager]: Loading Command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
 });
 
-// fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
+client.login(client.config.token);
 
-// point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars")
-  }
+
+
+app.post('/awake', (req, res) => {
+  res.sendStatus(200);
 });
 
-// load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-// Our home page route, this pulls from src/pages/index.hbs
-fastify.get("/", function(request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-  // check and see if someone asked for a random color
-  if (request.query.randomize) {
-    // we need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo
-    };
-  }
-  reply.view("/src/pages/index.hbs", params);
-});
-
-// A POST route to handle and react to form submissions 
-fastify.post("/", function(request, reply) {
-  let params = { seo: seo };
-  // the request.body.color is posted with a form submission
-  let color = request.body.color;
-  // if it's not empty, let's try to find the color
-  if (color) {
-    // load our color data file
-    const colors = require("./src/colors.json");
-    // take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-    // now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo
-      };
-    } else {
-      // try again.
-      params = {
-        colorError: request.body.color,
-        seo: seo
-      };
-    }
-  }
-  reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`server listening on ${address}`);
-});
+app.listen(3000, () => {
+  console.log('[EXPRESS] Started listening on port 3000');
+})
