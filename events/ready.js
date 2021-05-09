1f0d4c54425891c6876fe88e21107b9f1ede39d6
Glitch (glitch-hello-node) <none>
2021-05-09 10:05:44 +0000

    🎮❤️ Checkpoint ./events/ready.js:31490453/89

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
+client.user.setActivity("=help | https://twitch.tv/Z1ROXTV", {type: 'WATCHING'});
 };
+
