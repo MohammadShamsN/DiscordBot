198861d37318927a5b05924e8023656911764211
Glitch (glitch-hello-node) <none>
2021-05-11 15:16:55 +0000

    🏐🐦 Checkpoint ./commands/twitch.js:31490453/19

--- commands/twitch.js
+++ commands/twitch.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const commands = 
-   `Twitch : https://twitch.tv/Z1ROXTV` ;
+   ` :instagram: Twitch : https://twitch.tv/Z1ROXTV` ;
 
   const revised = commands
     .split("\n")
