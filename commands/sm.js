80ae9f9f498671af69314a1f31fd9a55f134101c
Glitch (glitch-hello-node) <none>
2021-05-12 08:41:27 +0000

    🍔🏖 Checkpoint ./commands/sm.js:31490453/598

--- commands/sm.js
+++ commands/sm.js
@@ -1,13 +1,13 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
-  const commands = `<:Instagram:736518226569068565> Instagram\ : https://Instagram.com/Z1ROXTV 
-   Twitch\ : <:twitch:760587569879711844> https://Twitch.tv/Z1ROXTV 
-   Youtube\ : <:YouTube:760587569385046098> https://www.youtube.com/channel/UCBKUnv2PAjeKqDpsgF2isbA
-   Donate\ : <:peepoScammer:841789232535961640> https://dono.gg/z1roxtv 
-   Telegram\ : <:Telegram:760587584220037160> https://t.me/Z1ROXTV 
-   Twitter\ : <:NM_Twitter:735279288743624714> https://twitter.com/Z1ROXTV
-   Discord\ : <:3702_party_discord:712764861997908040> https://discord.gg/FbcrFGmZkK`;
+  const commands = `<:Instagram:841790627549282435> Instagram\ : https://Instagram.com/Z1ROXTV 
+   <:Twitch:841790517460860968> Twitch\ : https://Twitch.tv/Z1ROXTV 
+   <:Youtube:841790561286357012> Youtube\ : https://www.youtube.com/channel/UCBKUnv2PAjeKqDpsgF2isbA
+   <:peepoScammer:841789232535961640> Donate\ : https://dono.gg/z1roxtv 
+   <:Telegram:841790593037238335> Telegram\ : https://t.me/Z1ROXTV 
+   <:Twitter:841790459239989248> Twitter\ : https://twitter.com/Z1ROXTV
+   <:Discord:841790487362928680> Discord\ : https://discord.gg/FbcrFGmZkK`;
    
   const revised = commands
     .split("\n")
