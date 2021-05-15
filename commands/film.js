34c8bbe37ec72e3567637dd82fd82a1e8fa3f9d8
Glitch (glitch-hello-node) <none>
2021-05-15 20:03:24 +0000

    🐫🐔 Checkpoint ./commands/film.js:31490453/642

--- commands/film.js
+++ commands/film.js
@@ -2,23 +2,12 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const commands = 
-   `@everyone : Name : Wrath Of Man
-
-ژانر: اکشن – هیجان انگیز
-
-محصول: انگلستان 2021
-
-کارگردان: Guy Ritchie
-
-بازیگران: Jason Statham, Niamh Algar, Josh Hartnett
-
-خلاصه داستان: یک شخصیت سرد و مرموز در یک شرکت حمل و نقل  کار می کند و مسئول انتقال صدها میلیون دلار به لوس آنجلس است اما…شما می توانید با تماشای فیلم و سریال های جدید اوقات زیبایی را سپری کنید با ما همراه باشید
-   https://cdn.discordapp.com/attachments/798169547454152704/843099738865795072/Wrath-of-Man-2021-4.jpg` ;
+   `@everyone Farda Shab Saat 11:30 ( 1 **Shanbe** ) Film ***Wrath Of Man*** Ro Mibinim !!!` ;
 
   const revised = commands
     .split("\n")
     .map((x) => x.trim())
     .join("\n");
 
-message.sent(revised);
+message.channel.send(revised);
 };
