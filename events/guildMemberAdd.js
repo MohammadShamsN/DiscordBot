9cf5158eb26b5225372fbf96f0c7329162f1e70e
Glitch (zirox) <none>
2021-07-15 21:14:26 +0000

    ✋🎰 Checkpoint ./.glitch-assets:31490453/512 ./events/guildMemberAdd.js:31490453/70

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -12,7 +12,7 @@ module.exports = async (client, member) => {
     const ctx = canvas.getContext('2d');
 
     // Background image
-    const background = await loadImage("https://cdn.glitch.com/7e9d342b-ad04-4e6e-8940-412aa3c82965%2Fbackground.png?v=1620494059517");
+    const background = await loadImage("https://cdn.glitch.com/7e9d342b-ad04-4e6e-8940-");
     ctx.drawImage(background, 0, 0, 750, 400);
 
     // Text
