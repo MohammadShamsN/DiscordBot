9efe1298f3bf8a675456d5911888500bed9caa0e
Glitch (zirox) <none>
2021-07-16 20:43:19 +0000

    👑😺 Checkpoint ./commands/cheese.js:24946265/10

--- commands/cheese.js
+++ commands/cheese.js
@@ -4,7 +4,7 @@ exports.run = async (client, message) => {
   message.channel.send(
       new MessageEmbed()
         .setAuthor(
-          " ",
+          "Socials",
           "https://cdn.discordapp.com/attachments/787804906006380605/865689028644175923/371903520_SOCIAL_ICONS_TRANSPARENT_400px.gif"
         )
         .setThumbnail("https://cdn.discordapp.com/attachments/787804906006380605/865689467850588180/tenor_1.gif")
