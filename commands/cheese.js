267e87680c15d70ba8d817d6b8fe7ccb6f0df944
Glitch (zirox) <none>
2021-07-16 20:31:19 +0000

    🏩🎾 Checkpoint ./commands/cheese.js:24946265/728

--- commands/cheese.js
+++ commands/cheese.js
@@ -1,12 +1,12 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
-  send(
+  message.channel.send(
       new MessageEmbed()
         .setAuthor(
-          "",
-          "https://img.icons8.com/color/2x/cd--v3.gif"
+          " ",
+          "https://cdn.discordapp.com/attachments/787804906006380605/865689028644175923/371903520_SOCIAL_ICONS_TRANSPARENT_400px.gif"
         )
-        .setThumbnail('')
+        .setThumbnail("https://cdn.discordapp.com/attachments/787804906006380605/865689467850588180/tenor_1.gif")
     );
 };
