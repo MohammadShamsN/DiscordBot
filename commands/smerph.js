c398d23988b3d2b73272913be692da31e9f96b65
Glitch (zirox) <none>
2021-07-16 23:33:24 +0000

    👵🐃 Checkpoint ./.glitch-assets:31490453/829 ./commands/cheese.js:31490453/412 ./commands/smerph.js:31490453/1130

--- commands/smerph.js
+++ commands/smerph.js
@@ -4,7 +4,9 @@ exports.run = async (client, message) => {
   message.channel.send(
       new MessageEmbed()
         .setAuthor(
-          "Socials",
+          "twitch https://www.twitch.tv/erph erphGachi",
+"youtube https://www.youtube.com/channel/UC26xLsVUPrKvRZJdjNehBRg erphGachi"
+          "Instagram https://www.instagram.com/erphhh/ erphGachi"
           "https://cdn.glitch.com/88d04f2d-1085-4493-a223-9abd169835de%2Fezgif.com-gif-maker.gif?v=1626477813676"
         )
         .setColor("F93CCA")
