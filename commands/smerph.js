eb9c4eeb40a2aeffd222b968b55778e59611b966
Glitch (zirox) <none>
2021-07-17 15:00:07 +0000

    🚴🚀 Checkpoint ./commands/cheese.js:24946265/348 ./commands/smerph.js:24946265/695

--- commands/smerph.js
+++ commands/smerph.js
@@ -4,12 +4,13 @@ exports.run = async (client, message) => {
   message.channel.send(
       new MessageEmbed()
         .setAuthor(
-          `twitch\ : https://twitch.tv/erph
-           youtube\ : https://youtube.com/channel/UC26xLsVUPrKvRZJdjNehBRg
-         Instagram\ : https://instagram.com/erphhh/`,
+          `Socials`,
           "https://cdn.glitch.com/88d04f2d-1085-4493-a223-9abd169835de%2FPicsArt_07-14-11.31.46.png?v=1626479060804"
         )
         .setColor("F93CCA")
         .setThumbnail("https://cdn.glitch.com/88d04f2d-1085-4493-a223-9abd169835de%2FPicsArt_07-14-11.31.46.png?v=1626479060804")
+        .addField("Twitch", "https://twitch.tv/erph")
+        .addField("Youtube", "https://youtube.com/channel/UC26xLsVUPrKvRZJdjNehBRg")
+        .addField("Instagram", "https://instagram.com/erphhh/")
     );
 };
