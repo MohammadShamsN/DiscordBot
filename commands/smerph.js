56f27c6cfc03b57e8f72557b3d3d6f4985af0c33
Glitch (zirox) <none>
2021-07-17 23:11:02 +0000

    🏦💑 Checkpoint ./commands/smerph.js:31490453/3171

--- commands/smerph.js
+++ commands/smerph.js
@@ -4,13 +4,14 @@ exports.run = async (client, message) => {
   message.channel.send(
       new MessageEmbed()
         .setAuthor(
-          `Socials`,
-          "https://cdn.glitch.com/88d04f2d-1085-4493-a223-9abd169835de%2FPicsArt_07-14-11.31.46.png?v=1626479060804"
+          `Erph Social Media`,
+           "https://cdn.glitch.com/88d04f2d-1085-4493-a223-9abd169835de%2F20210718_015659.gif?v=1626562688157"
         )
         .setColor("000000")
         .setThumbnail("https://cdn.glitch.com/88d04f2d-1085-4493-a223-9abd169835de%2FPicsArt_07-14-11.31.46.png?v=1626479060804")
         .addField("<a:Twitch:866048118214230036> Twitch", "https://twitch.tv/erph")
-        .addField("<a:Youtube:866044581212979201> Youtube", "https://youtube.com/channel/UC26xLsVUPrKvRZJdjNehBRg")
         .addField("<a:Insta:866047322537459763> Instagram", "https://instagram.com/erphhh/")
+        .addField("<a:Donate:866084777014984705> Donate", "https://reymit.com/erph")
+        .addField("<a:Youtube:866044581212979201> Youtube", "https://youtube.com/channel/UC26xLsVUPrKvRZJdjNehBRg")
     );
 };
