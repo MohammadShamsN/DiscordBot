d5cb4daafb9bd5e3d6671b040907aed20affe7d3
Glitch (zirox) <none>
2021-07-21 19:36:55 +0000

    💠🎯 Checkpoint ./commands/test.js:31490453/2062

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,16 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
-  message.channel.send(
-   ('<:discord:841959253082374145>: ```RISK```')
-   );
-};
+  const commands = <:Instagram:841790627549282435> Instagram : https://Instagram.com/Z1ROXTV 
+  <:Twitch:841790517460860968> Twitch : https://Twitch.tv/Z1ROXTV 
+   <:Youtube:841790561286357012> Youtube : https://www.youtube.com/channel/UCBKUnv2PAjeKqDpsgF2isbA
+   <:peepoScammer:841789232535961640> Donate : https://dono.gg/z1roxtv 
+   <:Telegram:841790593037238335> Telegram : https://t.me/Z1ROXTV 
+   <:Twitter:841790459239989248> Twitter : https://twitter.com/Z1ROXTV
+   <:discord:841959253082374145> Discord : https://discord.gg/FbcrFGmZkK' ;
+   
+  const revised = commands
+    .map((x) => + x.trim())
+    
+  message.channel.send(revised);
+  };
