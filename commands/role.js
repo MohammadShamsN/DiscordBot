eb582da8e12db4cb44ed8ee0f54928980b7b6de7
Glitch (zirox) <none>
2021-07-21 10:43:48 +0000

    💵🖼 Checkpoint ./commands/role.js:31490453/322

---
+++ commands/role.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (client, message) => {
+  const commands = 
+   '<:instagram:832195695557607424> : ```Twitch : https://twitch.tv/Z1ROXTV```' ;
+
+  const revised = commands
+    .split("\n")
+    .map((x) => x.trim())
+    .join("\n");
+
+message.reply(revised);
+}; 
