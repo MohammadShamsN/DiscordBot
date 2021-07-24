f6ad9d7b177834c59eab1eadcb127d28b4f66c23
Glitch (zirox) <none>
2021-07-24 14:10:05 +0000

    👆🌊 Checkpoint ./commands/test.js:31490453/509

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
-  const commands = 
-   '<:Twitch:841790517460860968> : ```Twitch```' ;
-
-  const revised = commands
-    .split("\n")
-    .map((x) => x.trim())
-    .join("\n");
-
-message.channel.send(revised);
+  const channel = message.channel;
+  channel.send('<:Twitch:841790517460860968> : `Twitch`\nMowz\n`test2`');
 };
