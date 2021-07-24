c068d20cbf6a2e9dc157b7f6cf24b044eb1e1e0c
Glitch (zirox) <none>
2021-07-24 14:22:04 +0000

    ☂️👨‍❤️‍💋‍👨 Checkpoint ./commands/test.js:31490453/561

--- commands/test.js
+++ commands/test.js
@@ -2,5 +2,5 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.channel;
-  channel.send('<:Twitch:841790517460860968> : `Twitch`\nMowz\n`test2`');
+  channel.send('<:Twitch:841790517460860968> : `Twitch`\n\n<:discord:841959253082374145> : `Discord`\n\n<:Youtube:841790561286357012> : `Youtube`\n\n<:Twitter:841790459239989248> : Twitter');
 };
