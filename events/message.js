f4f0f5e0c8e79341128fdcceab4790558edeeaff
Glitch (zirox) <none>
2021-07-30 22:53:11 +0000

    🏑🏖 Checkpoint ./events/message.js:31490453/108

--- events/message.js
+++ events/message.js
@@ -7,11 +7,11 @@ module.exports = (client, message) => {
     message.react(`<a:HesamGreetin:866306735420473375>`)
   }
   
-  if ( message.author.id  == `509982147918954507` && message.channel.id == `867084733753327636` ) {
+  if ( message.author.id  == `867087169638236230` && message.channel.id == `867084733753327636` ) {
     message.react(`<:ErphYEP:870764113926684722>`)
   }
  
-  if ( message.author.id == `582716832528465920` && message.channel.id == `867084733753327636` ) {
+  if ( message.author.id == `867087169638236230` && message.channel.id == `867084733753327636` ) {
     message.react(`<a:HesamGreetin:866306735420473375>`)
   }
   
