e7097faf6e110b58004659d3e46164647e363bce
Glitch (zirox) <none>
2021-07-30 20:56:52 +0000

    🚓🚋 Checkpoint ./events/message.js:31490453/540

--- events/message.js
+++ events/message.js
@@ -1,11 +1,19 @@
 module.exports = (client, message) => {
   if (message.author.bot) return;
   
-  if ( message.author.id  == `582716832528465920` && message.channel.id == `850325608243789864` ) {
+  if ( message.author.id  == `460515690760241153` && message.channel.id == `867084733753327636` ) {
     message.react(`<:VandalWTF:870764138522091531>`)
     message.react(`<a:hyperWOW:870764091411689483>`) 
   }
-
+  
+  if ( message.author.id  == `509982147918954507` && message.channel.id == `867084733753327636` ) {
+    message.react(`<:ErphYEP:870764113926684722>`)
+  }
+ 
+  if ( message.author.id == `582716832528465920` && message.channel.id == `867084733753327636` ) {
+    message.react(`<a:HesamGreetin:866306735420473375>`)
+  }
+  
   if (message.content.indexOf(client.config.prefix) !== 0) return
 
   const args = message.content
