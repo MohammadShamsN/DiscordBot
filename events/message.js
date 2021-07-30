e9fdab9c8bc97e0e7b3368c8b3f5881005ef0112
Glitch (zirox) <none>
2021-07-30 23:39:00 +0000

    👹🍢 Checkpoint ./events/message.js:31490453/54

--- events/message.js
+++ events/message.js
@@ -1,7 +1,7 @@
 module.exports = (client, message) => {
   if (message.author.bot) return;
   
-  if (message.webhookID == `867087169638236230` && message.channel.id == `867084733753327636`) {
+  if (message.webhook.id == `867087169638236230` && message.webhook.channelID == `867084733753327636`) {
     message.react(`<:VandalWTF:870764138522091531>`)
     message.react(`<a:hyperWOW:870764091411689483>`)
     message.react(`<a:HesamGreetin:866306735420473375>`)
