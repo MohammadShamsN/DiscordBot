a1a1655d745fa7d7cf50cd738026d2798eabbd46
Glitch (zirox) <none>
2021-07-30 19:51:28 +0000

    🚋🎍 Checkpoint ./events/message.js:31490453/233

--- events/message.js
+++ events/message.js
@@ -1,11 +1,10 @@
 module.exports = (client, message) => {
   if (message.author.bot) return;
-
-  if (message.content.indexOf(client.config.prefix) !== 0) return
   
   if ( message.author.id == `582716832528465920` ) {
     message.react(`<a:HesamGreetin:866306735420473375>`)
   }
+  if (message.content.indexOf(client.config.prefix) !== 0) return
 
   const args = message.content
     .slice(client.config.prefix.length)
