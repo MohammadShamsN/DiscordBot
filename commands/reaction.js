0554d5c5dccdd2cd45c7a29b78d5528940103b51
Glitch (bote-erph) <none>
2021-08-01 22:46:21 +0000

    🏝👢 Checkpoint ./commands/reaction.js:31490453/972

---
+++ commands/reaction.js
@@ -0,0 +1,6 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (client, message) => {
+  const channel = message.channel;
+  channel.send(' : `Dota`\n\n : `Fortnite`\n\n : `VALORANT`\n\n : `COD Warzone`\n\n : `Apex Legends`\n\n : `Sea Of Thieves`\n\n : `Risk`\n\n : `Amoung Us`\n\n : `Mafia`\n\n : `Sport`\n\n : `Cinema`');
+};
