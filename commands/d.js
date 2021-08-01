71e629bf3711bd32d07fbe53944cc0fb29ab7d29
Glitch (bote-erph) <none>
2021-08-01 19:19:26 +0000

    🌁🐣 Checkpoint ./commands/d.js:31490453/40

--- commands/d.js
+++ commands/d.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const commands = 
-  'Discord : https://discord.gg/3Pu2QppU4n' ;
+  '<a:Discord2:871469574434988072> https://discord.gg/3Pu2QppU4n' ;
 
   const revised = commands
     .split("\n")
