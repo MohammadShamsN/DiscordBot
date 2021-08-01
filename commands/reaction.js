10249c3ab29d7c234ff9d7944201e23121e32109
Glitch (bote-erph) <none>
2021-08-01 23:32:10 +0000

    🏌📅 Checkpoint ./commands/reaction.js:31490453/4563

--- commands/reaction.js
+++ commands/reaction.js
@@ -2,5 +2,5 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.channel;
-  channel.send(' : `Dota`\n\n : `Fortnite`\n\n : `VALORANT`\n\n : `CSGO`\n\n : `COD Warzone`\n\n : `Apex Legends`\n\n : `Sea Of Thieves`\n\n : `Risk`\n\n : `Amoung Us`\n\n : `Mafia`\n\n : `Sport`\n\n : `Cinema`');
+  channel.send('<:Dota:871526772649373766> : `Dota2`\n\n<:Fortnite:871526724079337572> : `Fortnite`\n\n<:Valo:871526749719130204> : `VALORANT`\n\n<:CSGO:871528335522537532> :  `CSGO`\n\n<:COD:871526350337474591> : `COD Warzone`\n\n<:ApexLegends:871526605607030885> : `Apex Legends`\n\n<:SOT:871526638431653948> : `Sea Of Thieves`\n\n<:Risk:871526675719008316> : `Risk`\n\n<:Amongus:871526327323353118> : `Amoung Us`\n\n<:Mafia:871526385846468638> : `Mafia`\n\n : `Sport`\n\n : `Cinema`');
 };
