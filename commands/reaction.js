3b76d2fbdbb37593e3769bc4e3955090bb5298fa
Glitch (bote-erph) <none>
2021-08-02 00:18:43 +0000

    🍥🍳 Checkpoint ./commands/reaction.js:31490453/128

--- commands/reaction.js
+++ commands/reaction.js
@@ -2,5 +2,5 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.channel;
-  channel.send('<:Dota:871526772649373766> : `Dota2`\n\n<:Fortnite:871526724079337572> : `Fortnite`\n\n<:Valo:871526749719130204> : `VALORANT`\n\n<:CSGO:871528335522537532> :  `CSGO`\n\n<:COD:871526350337474591> : `COD Warzone`\n\n<:ApexLegends:871526605607030885> : `Apex Legends`\n\n<:SOT:871526638431653948> : `Sea Of Thieves`\n\n<:Risk:871526675719008316> : `Risk`\n\n<:Amongus:871526327323353118> : `Amoung Us`\n\n<:Mafia:871526385846468638> : `Mafia`\n\n : `Sport`\n\n : `Cinema`');
+  channel.send('<:Dota:871526772649373766> : `Dota2`\n\n<:Fortnite:871526724079337572> : `Fortnite`\n\n<:Valo:871526749719130204> : `VALORANT`\n\n<:CSGO:871528335522537532> :  `CSGO`\n\n<:COD:871526350337474591> : `COD Warzone`\n\n<:ApexLegends:871526605607030885> : `Apex Legends`\n\n<:SOT:871526638431653948> : `Sea Of Thieves`\n\n<:Risk:871526675719008316> : `Risk`\n\n<:Amongus:871526327323353118> : `Amoung Us`\n\n<:Mafia:871543496136327168> : `Mafia`\n\n<:toop:871536288614993961> : `Sport`\n\n<:cinema:871539814284029974> : `Cinema`');
 };
