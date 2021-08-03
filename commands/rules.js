c584f2b9bdfcaafff0908cd23c00de47133b7584
Glitch (bote-erph) <none>
2021-08-03 22:41:56 +0000

    👛⛑ Checkpoint ./commands/rules.js:31490453/1173 ./commands/role.js:31490453/119

---
+++ commands/rules.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (client, message) => {
+` Salam khedmate hame doostane azize server .
+
+Lotfan ghavanino ba **deghat** motale'e konid .
+
+:one: Spam nakonid .
+
+:two: Bahse siasi, maz'habi va nezhad parastane mamnooe .
+
+:three: be hamdige ehteram bezarid va az be kar bordane  kalamate rakik khoddari konid .
+
+:four: Az ferestadane payam haye naa-marboot dar channel haye mokhtalef khoddari konid .
+
+:five: Rulese har channel dakhale topicesh neveshte shode hatman topice har channelo motale'e konid
+
+:six: Baraye member haye channel mozahemat injad nakonid, hatta dar DM .
+
+:seven: Tabligh nakonid. Faghat mojaz be gozashtane link haye marboot be contente khodetoon hastid, oonam faghat dar channele #⬆promotion  .
+
+:eight: Dar morede streamer ha va youtuber haye dige sohbat nakonid .
+
+Dar soorati ke ghavanino ra'ayat nakonin, **Ba shoma barkhord khahad shod* .`
+};
