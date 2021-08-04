e840a9e4c728315013ce63be831c03d8f0d46ea0
Glitch (bote-erph) <none>
2021-08-04 23:43:33 +0000

    🎱💂 Checkpoint ./events/ready.js:31490453/87

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
 
-  client.user.setActivity("Feghat 1 Roze Dige PauseChamp", {type: 'PLAYING'});
+  client.user.setActivity("Tarikh Start Stream Jome Saat 8 https://twitch.tv/erph", {type: 'PLAYING'});
 };
 
