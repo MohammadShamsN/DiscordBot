320e2d6e7257ed3d05eb806389a90dd828ae08f5
Glitch (bote-erph) <none>
2021-08-05 07:43:10 +0000

    🚕🍀 Checkpoint ./events/ready.js:31490453/1

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
 
-  client.user.setActivity("Jome Saat 20 ", {type: 'PLAYING'});
+  client.user.setActivity("Jome Saat 20", {type: 'PLAYING'});
 };
 
