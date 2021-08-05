989efe3e9ff5922504865d99c5520f1a2fde5b9e
Glitch (bote-erph) <none>
2021-08-05 00:30:35 +0000

    📹🚧 Checkpoint ./commands/r.js:31490453/2121

--- commands/r.js
+++ commands/r.js
@@ -2,28 +2,28 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const commands = 
-   `Salam khedmate hame doostane azize server.
-Lotfan ghavanino ba **deghat** motale'e konid.
+   `Salam Khedmate Hame Doostane Azize Server.
+Lotfan Ghavanino Ba **Deghat** Motale'e Konid.
 
 
-**1.** Spam nakonid.
+**1.** Spam Nakonid.
 
-**2.** Bahse siasi, maz'habi va nezhad parastane mamnooe.
+**2.** Bahse Siasi, Maz'habi Va Nezhad Parastane Mamnooe.
 
-**3.** be hamdige ehteram bezarid va az be kar bordane  kalamate rakik khoddari konid.
+**3.** Be Hamdige Ehteram Bezarid Va Az Be Kar Bordane  Kalamate Rakik Khoddari Konid.
 
-**4.** Az ferestadane payam haye naa-marboot dar channel haye mokhtalef khoddari konid.
+**4.** Az Ferestadane Payam Haye Naa-Marboot Dar Channel Haye Mokhtalef Khoddari Konid.
 
-**5.** Ghavanin har channel dakhale topicesh neveshte shode hatman topice har channelo motale'e konid.
+**5.** Ghavanin Har Channel Dakhale Topicesh Neveshte Shode Hatman Topice Har Channelo Motale'e Konid.
 
-**6.** Baraye member haye channel mozahemat injad nakonid, hatta dar DM.
+**6.** Baraye Member Haye Channel Mozahemat Injad Nakonid, Hatta Dar DM.
 
-**7.** Tabligh nakonid. Faghat mojaz be gozashtane link haye marboot be contente khodetoon hastid, oonam faghat dar channele #promotion 
+**7.** Tabligh Nakonid. Faghat Mojaz Be Gozashtane Link Haye Marboot Be Contente Khodetoon Hastid, Oonam Faghat Dar Channele **Promotion**.
 
-**8.** Dar morede streamer ha va youtuber haye dige sohbat nakonid.
+**8.** Dar Morede Streamer Ha Va Youtuber Haye Dige Sohbat Nakonid.
 
 
-Dar soorati ke ghavanino ra'ayat nakonin, **Ba shoma barkhord khahad shod**.`
+Dar Soorati Ke Ghavanino Ra'ayat Nakonin, **Ba Shoma Barkhord Khahad Shod**.`
 
   const revised = commands
     .split("\n")
