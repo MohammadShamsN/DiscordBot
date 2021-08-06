8012e91d08d71f9a2705de8d6c38a2a746d66f68
Glitch (bote-erph) <none>
2021-08-06 17:28:20 +0000

    ⛲️🌈 Checkpoint ./events/ready.js:31490453/56

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
 
-  client.user.setActivity("Emroz Saat 20", {type: 'PLAYING'});
+  client.user.setActivity("Youtube Sub Konid Farda Video Darim", {type: 'PLAYING'});
 };
 
