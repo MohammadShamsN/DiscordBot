0a5a6afe03dd49cfa5cce093c2f36888ba714936
Glitch (bote-erph) <none>
2021-08-07 12:30:40 +0000

    🎃🚔 Checkpoint ./package.json:24946265/274 ./events/ready.js:31490453/126

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
 
-  client.user.setActivity("Bezoodi Video Youtube Darim", {type: 'PLAYING'});
+  client.user.setActivity("Emashab Saat8 Video Youtube", {type: 'PLAYING'});
 };
 
