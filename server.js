07b39ca0d6bf9c41c9132c8e87631db8b864aabd
Glitch (bote-erph) <none>
2021-08-07 02:07:07 +0000

    📉⛵️ Checkpoint ./server.js:31490453/2398 ./package.json:31490453/23

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const Discord = require("discord.js");
 const fs = require("fs");
 require("dotenv").config();
 
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
 
 const client = new Discord.Client();
 
 const config = {
   token: process.env.TOKEN,
-  prefix: process.env.PREFIX,
+  prefix: process.env.PREFIX
 };
 client.config = config;
 client.queue = new Map();
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
-  files.forEach((file) => {
+  files.forEach(file => {
     const event = require(`./events/${file}`);
     let eventName = file.split(".")[0];
     client.on(eventName, event.bind(null, client));
@@ -28,7 +28,7 @@ client.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
-  files.forEach((file) => {
+  files.forEach(file => {
     if (!file.endsWith(".js")) return;
     let props = require(`./commands/${file}`);
     let commandName = file.split(".")[0];
@@ -39,8 +39,8 @@ fs.readdir("./commands/", (err, files) => {
 
 client.login(client.config.token);
 
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 setInterval(async () => {
-  await fetch('https://bote-erph.glitch.me').then(console.log('Pinged'))
-},240000)
+  await fetch("https://bote-erph.glitch.me").then(console.log("Pinged"));
+}, 240000);
