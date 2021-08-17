c42cf6cbaf1d352e174118cb8b7ed0a6e7b5dbe9
Glitch (bote-erph) <none>
2021-08-17 16:17:48 +0000

    🌔🍶 Checkpoint ./package.json:31490453/4 ./events/ready.js:31490453/62

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
 
-  client.user.setActivity("Video Jadid Upload Shod Link To About me", {type: 'WATCHING'});
+  client.user.setActivity("🎂 HBD Erph", {type: 'PLAYING'});
 };
 
