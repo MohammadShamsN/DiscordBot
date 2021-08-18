22cf0bd74055a91290e6f2b9ecb86f4cf96005f5
Glitch (bote-erph) <none>
2021-08-18 18:49:20 +0000

    📅📰 Checkpoint ./commands/ban.js:24946265/650

--- commands/ban.js
+++ commands/ban.js
@@ -4,22 +4,21 @@ exports.run = (client, message) => {
     if(!message.guild.members.cache.find(user => user.id == client.user.id).hasPermission('BAN_MEMBERS'))
       return message.reply("You don't have the permission to ban people bitch. Get the hell outa here.");
   
-    /*if(!message.guild.me.hasPermission('BAN_MEMBERS'))
+    if(!message.guild.me.hasPermission('BAN_MEMBERS'))
       return message.reply("I don't have the permission to ban. :(");
   
-    const args = message.content.slice((process.env.prefix).length).trim().split(/ +/g);
-    const username = args[0];
-    const user = client.users.cache.find(user => user.username == username);
-    if(user == null)
-        return message.reply("User not found!");
+    const args = message.content.slice((process.env.PREFIX).length).trim().split(/ +/g);
+    const user = message.mentions.users.first();
+    if(user == undefined)
+        return message.reply("You need to mention the user!");
   
-    const reason = args[1];
-    if(reason == null)
-        reason = "";
+    const reason = args[2];
+    if(reason == undefined)
+        return message.reply("You need to specify a reason.");
   
     
     message.guild.members.ban(user.id)
-        .then(kickInfo => console.log(`Banned ${user}`))
-        .catch(console.error);*/
+        .then(kickInfo => message.reply(`Banned ${user}\nReason: ${reason}\nGet rekt bitch.`))
+        .catch(console.error);
   
 };
