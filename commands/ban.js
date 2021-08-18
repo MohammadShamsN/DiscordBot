7497d083933378e749101be2749f0d73bdcb329d
Glitch (bote-erph) <none>
2021-08-18 18:11:12 +0000

    🐠💖 Checkpoint ./commands/ban.js:24946265/527

--- commands/ban.js
+++ commands/ban.js
@@ -1,5 +1,15 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = message.content.slice((process.env.prefix).length).trim().split(/ +/g);
+    const username = args[0];
+    const user = client.users.cache.find(user => user.username == username);
+    if(user == null)
+        return client.send("User not found!");
+  
+    const reason = args[1];
+    if(reason == null)
+        reason = "";
+  
+  
 };
