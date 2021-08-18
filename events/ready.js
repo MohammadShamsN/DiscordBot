f4e4af8bf9463312c076da78686932f598a4d661
Glitch (bote-erph) <none>
2021-08-18 16:27:37 +0000

    👔🤗 Checkpoint ./events/ready.js:31490453/21

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
 
-  client.user.setActivity("📱 Social Media", {type: 'WATCHIING'});
+  client.user.setActivity("📱 Social Media", {type: 'WATCHING'});
 };
 
