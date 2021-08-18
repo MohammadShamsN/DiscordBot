8bc27a7ac682806fcce763b9fcbaf34267e74e5e
Glitch (bote-erph) <none>
2021-08-18 18:22:50 +0000

    🍴🍚 Checkpoint ./commands/ban.js:24946265/1009

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,12 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
+    if(!message.guild.members.cache.find(client.user.id).hasPermission('BAN_MEMBERS'))
+      return client.send("You don't have the permission to ban people bitch. Get the hell outa here.");
+  
+    if(!message.guild.me.hasPermission('BAN_MEMBERS'))
+      return client.send("I don't have the permission to ban. :(");
+  
     const args = message.content.slice((process.env.prefix).length).trim().split(/ +/g);
     const username = args[0];
     const user = client.users.cache.find(user => user.username == username);
@@ -11,5 +17,11 @@ exports.run = async (client, message) => {
     if(reason == null)
         reason = "";
   
+    var kickInfo = {
+      
+    };
+    message.guild.members.ban(user.id)
+        .then(kickInfo => console.log(`Banned ${kickInfo.user?.tag ?? kickInfo.tag ?? kickInfo}`))
+        .catch(console.error);
   
 };
