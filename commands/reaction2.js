71f89de1c4ff69b65c4ec88ac3f6d7d302d2e6a0
Glitch (bote-erph) <none>
2021-09-27 22:16:42 +0000

    🦀👌 Checkpoint ./commands/reaction2.js:31490453/225

--- commands/reaction2.js
+++ commands/reaction2.js
@@ -2,5 +2,6 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.channel;
-client.channels.cache.get(`848313858254962728`).fetchMessage(`892160224717307975`).then(msg => msg.delete());
+
+guild.channels.get(channelID).fetchMessage(messageID).edit('New Content')
 };
