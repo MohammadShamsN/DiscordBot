12df17c50f3ec013789d6c0e8c464dd62c03f9fe
Glitch (bote-erph) <none>
2021-10-02 10:56:00 +0000

    🚇🚛 Checkpoint ./commands/smp.js:31490453/211

--- commands/smp.js
+++ commands/smp.js
@@ -4,11 +4,10 @@ exports.run = async (client, message) => {
   message.channel.send(
       new MessageEmbed()
         .setAuthor(
-          `Saeedp7 Social Media`,
+          `Follow Me On Our Social Media`,
            "https://cdn.glitch.com/88d04f2d-1085-4493-a223-9abd169835de%2F20210718_155407.gif?v=1626612977276"
-        )
-        .setTitle("Follow Me On Our Social Media")
-        .setColor("CD00FF")
+        ) 
+        .setColor("08fcfc")
         .setThumbnail("https://media.discordapp.net/attachments/825067075818356796/893790112868098128/PicsArt_10-02-12.21.31.png")
         .addField("<a:Twitch2:866048118214230036> Twitch", "https://twitch.tv/Saeedp7")
         .addField("<a:Insta2:866274244131291156> Instagram", "https://instagram.com/Saeedp7")
