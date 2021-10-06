a7ffa88afb518b0ee3b2b01380f6fd8a8afa5e0d
Glitch (bote-erph) <none>
2021-10-06 18:11:14 +0000

    🏕⚾️ Checkpoint ./commands/reaction2.js:31490453/537

--- commands/reaction2.js
+++ commands/reaction2.js
@@ -2,5 +2,5 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.channel;
-  channel.messages.cache.find(message => message.id == '893815630074085406').then(message => message.edit("Khaste Nabashid!"));
+  channel.messages.cache.find(msg=> msg.id == '893815630074085406').then(msg => message.edit("Khaste Nabashid!"));
 };
