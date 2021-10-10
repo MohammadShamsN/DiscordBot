3c918c0f986ddc67c135bcb1d7a7aafc30732057
Glitch (bote-erph) <none>
2021-10-10 12:00:13 +0000

    🎳📈 Checkpoint ./commands/reaction2.js:24946265/661:31490453/1550

--- commands/reaction2.js
+++ commands/reaction2.js
@@ -2,11 +2,13 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.channel;
-  var myMsg = channel.messages.fetch("896721365095305267")
-  if(myMsg == undefined)
-    console.log("ERROR: Message not found!")
-  else if (myMsg.editable == false)
-    console.log("ERROR: I cannot edit this message!")
-  else
-    myMsg.edit("Khaste Nabashid!");
+  var myMsg = channel.messages.fetch("896726475754053664").then(msg => {  //    <------------ ID message ro inja bezar
+    if(msg == undefined)
+      console.log("ERROR: Message not found!")
+    else if(msg.editable == false)
+      console.log("ERROR: Message is not editable!")
+    else
+      msg.edit('Baraye Daryaft Kardane Role **Game** Va **Sport** Va **Cinema** Roye Reacte Haye Zir Bezanid !\n\n<:Dota:871526772649373766> : `Dota 2`\n\n<:Fortnite:871526724079337572> : `Fortnite`\n\n<:Valo:871526749719130204> : `VALORANT`\n\n<:CSGO:871810054595182672> :  `CS:GO`\n\n<:COD:871816083730989146> : `COD: Warzone`\n\n<:ApexLegends:871526605607030885> : `Apex Legends`\n\n<:SOT:871526638431653948> : `Sea of Thieves`\n\n<:Risk:871526675719008316> : `RISK`\n\n<:Amongus:871526327323353118> : `Among Us`\n\n<:Mafia:872782118806253619> : `Mafia`\n\n<:toop:871536288614993961> : `Sports`\n\n<:cinema:871539814284029974> : `Cinema`\n\n `kheste nebeshen`')
+  })
+  
 };
