05b5af5395832c83f375ee5f450ed8131d7c57f0
Glitch (bote-erph) <none>
2021-10-10 13:54:44 +0000

    📡🏬 Checkpoint ./events/messageDelete.js:24946265/1072

---
+++ events/messageDelete.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = async (client, message) => {
+  const guild = message.guild;
+  const member = message.member;
+  // ----------------------- [Logging] ----------------------- 
+  var avatar = member.user.displayAvatarURL();
+  
+  const logMessage = new MessageEmbed()
+	.setColor('#0099ff')
+	.setAuthor(member.displayName, avatar, null) // user info
+	.setDescription(member.user.tag + ` deleted message in ${message.channel}: ${message}`) // message
+	.setThumbnail(avatar) // user profile
+	.setTimestamp();
+
+  var channel = guild.channels.cache.get(process.env.LOG_CHANNEL_ID);
+  if(channel == undefined)
+      console.log('ERROR: Channel not found.')
+    else
+      channel.send(logMessage);
+  // ---------------------------------------------------------
+  
+}
